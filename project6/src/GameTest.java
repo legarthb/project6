ba0a0e8e9a547c922d765850faccab6525fdac90
Tommy <thetfordt18@mail.wlu.edu>
2017-11-05 15:04:46 -0500

    commit

--- project6/src/GameTest.java
+++ project6/src/GameTest.java
@@ -1,6 +1,6 @@
 
-public class GameTest {
-	public static void main(String[] args) {
+public class GameTest{
+    public static void main(String[] args) {
 		GameModel game = new GameModel();
 		game.Begin();
 		game.toString();
